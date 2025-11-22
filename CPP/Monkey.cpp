00c136204e08cc5043f71fa483c0da98d5d0dff8
modabbir553 <modabbir553@gmail.com>
2025-11-22 19:11:51 +0600

    solve the prblm for monkey

--- CPP/Monkey.cpp
+++ CPP/Monkey.cpp
@@ -6,31 +6,24 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
+
     char str1[100001];
-    char str2[100001];
-    cin.getline(str1, 100001);
-    cin.getline(str2, 100001);
 
-    int l1 = strlen(str1);
-    int l2 = strlen(str2);
-    sort(str1, str1 + l1);
-    sort(str2, str2 + l2);
-
-    for (int i = 0; i < l1; i++)
+    while (cin.getline(str1, 100001))
     {
-        if (str1[i] != ' ')
-        {
-            cout << str1[i];
-        }
-    }
-    cout << endl;
+        int l1 = strlen(str1);
 
-    for (int i = 0; i < l2; i++)
-    {
-        if (str2[i] != ' ')
+        sort(str1, str1 + l1);
+
+        for (int i = 0; i < l1; i++)
         {
-            cout << str2[i];
+            if (str1[i] != ' ')
+            {
+                cout << str1[i];
+            }
         }
+        cout << endl;
     }
+
     return 0;
 }
---
+++ CPP/Who_Is_It.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Student_info
+{
+public:
+    int id;
+    string name;
+    char section;
+    double marks;
+
+    Student_info(int s_id, string s_name, char s_section, double s_mark)
+    {
+        id = s_id;
+        name = s_name;
+        section = s_section;
+        marks = s_mark;
+    }
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    for(int i = 0; i<t; i++){
+        Student_info obj1()
+    }
+    return 0;
+}
