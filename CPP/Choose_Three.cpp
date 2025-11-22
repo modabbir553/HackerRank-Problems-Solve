34cb802fa16353efeca3edd0b3d93d3f4cb5a06b
modabbir553 <modabbir553@gmail.com>
2025-11-22 20:27:24 +0600

    solve a new prblm

--- CPP/Choose_Three.cpp
+++ CPP/Choose_Three.cpp
@@ -1,34 +1,41 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-ios_base::sync_with_stdio(false);
-cin.tie(0);
-cout.tie(0);
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
     int t;
     cin >> t;
-    int n,s;
-    while(t--){
+    int n, s;
+    while (t--)
+    {
         int temp = 0;
         int result = 0;
         cin >> n >> s;
         int ar[n];
-        for(int i = 0; i<n; i++){
+        for (int i = 0; i < n; i++)
+        {
             cin >> ar[i];
         }
 
-        sort(ar, ar+n, greater<int>());
-        for(int j = n-1; j>=0; j--){
-            
+        sort(ar, ar + n, greater<int>());
+        for (int j = n - 1; j >= 0; j--)
+        {
+
             temp = temp + ar[j];
-            if(temp <= s){
+            if (temp <= s)
+            {
                 result = temp;
             }
         }
-        if(result== s){
+        if (result == s)
+        {
             cout << "YES" << endl;
         }
-        else{
+        else
+        {
             cout << "NO" << endl;
         }
     }
